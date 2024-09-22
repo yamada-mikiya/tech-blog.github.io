fc6b82fe20ed3c52ba18b87a3b395454ea82f07b
yamada-mikiya <2415015t@cloud.kobe-u.jp>
2024-09-22 16:08:31 +0900

    setup ogp

--- setting.ts
+++ setting.ts
@@ -1,2 +1,3 @@
 // ex) export const TARGET_URL = "https://xxxx.github.io/tech-blog.github.io/";
-export const TARGET_URL = "https://";
+export const TARGET_URL =
+  "https://yamada-mikiya.github.io/tech-blog.github.io/";
